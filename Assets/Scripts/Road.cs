427d8e9fe92cbf648466720848c02b5a914c71fc
Dan Boehmke <Danboe2009@gmail.com>
2016-05-03 16:40:44 -0400

    Road Repeater Mach 1

---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Road : MonoBehaviour {
+
+	private float speed = -3f;
+
+	private Rigidbody2D myBody;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		myBody.velocity = new Vector2 (0f, speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log(other.gameObject);
+		if (other.gameObject.tag == "Repeater") {
+			transform.position = new Vector3 (-1.82f, 10.24f, 0f);
+		}
+	}
+
+}
